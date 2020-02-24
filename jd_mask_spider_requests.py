3f828551307cbadf792775c5c57c09ad03251251
Chowxj <Chow18566476252@163.com>
2020-02-24 14:16:20 +0800

    base versions

---
+++ jd_mask_spider_requests.py
@@ -0,0 +1,296 @@
+import random
+from datetime import datetime
+import sys
+import time
+from jd_mask.jdlogger import logger
+from jd_mask.timer import Timer
+import requests
+from jd_mask.util import parse_json, get_session, DEFAULT_USER_AGENT, get_sku_title
+from prompt_toolkit import prompt
+from jd_mask.config import global_config
+
+class Jd_Mask_Spider(object):
+    def __init__(self):
+        # 初始化session
+        self.session = get_session()
+        self.sku_id = global_config.getRaw('config', 'sku_id')
+        self.seckill_init_info = dict()
+        self.seckill_url = dict()
+        self.seckill_order_data = dict()
+
+    def login(self):
+        for flag in range(1, 3):
+            try:
+                targetURL = 'https://order.jd.com/center/list.action'
+                payload = {
+                    'rid': str(int(time.time() * 1000)),
+                }
+                resp = self.session.get(
+                    url=targetURL, params=payload, allow_redirects=False)
+                if resp.status_code == requests.codes.OK:
+                    logger.info('校验是否登录[成功]')
+                    logger.info('用户:{}'.format(self.get_username()))
+                    return True
+                else:
+                    logger.info('校验是否登录[失败]')
+                    logger.info('请从新输入cookie')
+                    time.sleep(1)
+                    continue
+            except Exception as e:
+                logger.info('第【%s】次失败请重新获取cookie', flag)
+                time.sleep(1)
+                continue
+        sys.exit(1)
+
+    def make_reserve(self):
+        """商品预约"""
+        logger.info('商品名称:{}'.format(get_sku_title()))
+        logger.info('预购时间例子:2020-02-17 18:00:00.100')
+        appointment_time = prompt('请输入需要抢预购的时间:')  # 2020-02-17 19:00:01.100
+        url = 'https://yushou.jd.com/youshouinfo.action?'
+        payload = {
+            'callback': 'fetchJSON',
+            'sku': self.sku_id,
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/531.36',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        resp = self.session.get(url=url, params=payload, headers=headers)
+        resp_json = parse_json(resp.text)
+        reserve_url = resp_json.get('url')
+        logger.info(reserve_url)
+        timers = Timer(appointment_time)
+        timers.start()
+        while True:
+            try:
+                self.session.get(url='https:' + reserve_url)
+                logger.info('总共预约使用时间:{}s'.format(time.time()))
+                logger.info('预约成功，已获得抢购资格 / 您已成功预约过了，无需重复预约')
+                logger.info('结束时间为:{}'.format(datetime.now()))
+                logger.info(
+                    '总共使用时间:{}'.format(
+                        datetime.now() -
+                        timers.buy_time))
+                break
+            except Exception as e:
+                logger.error('预约失败正在重试...')
+
+    def get_username(self):
+        """获取用户信息
+        :return: 用户名
+        """
+        url = 'https://passport.jd.com/user/petName/getUserInfoForMiniJd.action'
+        payload = {
+            'callback': 'jQuery'.format(random.randint(1000000, 9999999)),
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/531.36',
+            'Referer': 'https://order.jd.com/center/list.action',
+        }
+        try:
+            resp = self.session.get(url=url, params=payload, headers=headers)
+            resp_json = parse_json(resp.text)
+            # many user info are included in response, now return nick name in it
+            # jQuery2381773({"imgUrl":"//storage.360buyimg.com/i.imageUpload/xxx.jpg","lastLoginTime":"","nickName":"xxx","plusStatus":"0","realName":"xxx","userLevel":x,"userScoreVO":{"accountScore":xx,"activityScore":xx,"consumptionScore":xxxxx,"default":false,"financeScore":xxx,"pin":"xxx","riskScore":x,"totalScore":xxxxx}})
+            return resp_json.get('nickName') or 'jd'
+        except Exception:
+            return 'jd'
+
+    def get_seckill_url(self):
+        """获取商品的抢购链接
+        点击"抢购"按钮后，会有两次302跳转，最后到达订单结算页面
+        这里返回第一次跳转后的页面url，作为商品的抢购链接
+        :param sku_id: 商品id
+        :return: 商品的抢购链接
+        """
+        url = 'https://itemko.jd.com/itemShowBtn'
+        payload = {
+            'callback': 'jQuery{}'.format(random.randint(1000000, 9999999)),
+            'skuId': self.sku_id,
+            'from': 'pc',
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': DEFAULT_USER_AGENT,
+            'Host': 'itemko.jd.com',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        while True:
+            resp = self.session.get(url=url, headers=headers, params=payload)
+            resp_json = parse_json(resp.text)
+            if resp_json.get('url'):
+                # https://divide.jd.com/user_routing?skuId=8654289&sn=c3f4ececd8461f0e4d7267e96a91e0e0&from=pc
+                router_url = 'https:' + resp_json.get('url')
+                # https://marathon.jd.com/captcha.html?skuId=8654289&sn=c3f4ececd8461f0e4d7267e96a91e0e0&from=pc
+                seckill_url = router_url.replace(
+                    'divide', 'marathon').replace(
+                    'user_routing', 'captcha.html')
+                logger.info("抢购链接获取成功: %s", seckill_url)
+                return seckill_url
+            else:
+                logger.info("抢购链接获取失败，%s不是抢购商品或抢购页面暂未刷新，1秒后重试")
+                time.sleep(1)
+
+    def request_seckill_url(self):
+        """访问商品的抢购链接（用于设置cookie等）
+        :param sku_id: 商品id
+        :return:
+        """
+        logger.info('用户:{}'.format(self.get_username()))
+        logger.info('商品名称:{}'.format(get_sku_title()))
+        logger.info('预购时间例子:2020-02-17 18:00:00.100')
+        appointment_time = prompt('请输入需要抢预购的时间:')  # 2020-02-17 19:00:01.100
+        timers = Timer(appointment_time)
+        timers.start()
+        self.seckill_url[self.sku_id] = self.get_seckill_url()
+        logger.info('访问商品的抢购连接...')
+        headers = {
+            'User-Agent': DEFAULT_USER_AGENT,
+            'Host': 'marathon.jd.com',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        self.session.get(
+            url=self.seckill_url.get(
+                self.sku_id),
+            headers=headers,
+            allow_redirects=False)
+
+    def request_seckill_checkout_page(self):
+        """访问抢购订单结算页面
+        :param sku_id: 商品id
+        :param num: 购买数量，可选参数，默认1个
+        :return:
+        """
+        logger.info('访问抢购订单结算页面...')
+        url = 'https://marathon.jd.com/seckill/seckill.action'
+        payload = {
+            'skuId': self.sku_id,
+            'num': 1,
+            'rid': int(time.time())
+        }
+        headers = {
+            'User-Agent': DEFAULT_USER_AGENT,
+            'Host': 'marathon.jd.com',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        self.session.get(url=url, params=payload, headers=headers)
+
+    def _get_seckill_init_info(self):
+        """获取秒杀初始化信息（包括：地址，发票，token）
+        :param sku_id:
+        :param num: 购买数量，可选参数，默认1个
+        :return: 初始化信息组成的dict
+        """
+        logger.info('获取秒杀初始化信息...')
+        url = 'https://marathon.jd.com/seckillnew/orderService/pc/init.action'
+        data = {
+            'sku': self.sku_id,
+            'num': 1,
+            'isModifyAddress': 'false',
+        }
+        headers = {
+            'User-Agent': DEFAULT_USER_AGENT,
+            'Host': 'marathon.jd.com',
+        }
+        resp = self.session.post(url=url, data=data, headers=headers)
+        return parse_json(resp.text)
+
+    def _get_seckill_order_data(self):
+        """生成提交抢购订单所需的请求体参数
+        :param sku_id: 商品id
+        :param num: 购买数量，可选参数，默认1个
+        :return: 请求体参数组成的dict
+        """
+        logger.info('生成提交抢购订单所需参数...')
+        # 获取用户秒杀初始化信息
+        self.seckill_init_info[self.sku_id] = self._get_seckill_init_info()
+        init_info = self.seckill_init_info.get(self.sku_id)
+        default_address = init_info['addressList'][0]  # 默认地址dict
+        invoice_info = init_info.get('invoiceInfo', {})  # 默认发票信息dict, 有可能不返回
+        token = init_info['token']
+        data = {
+            'skuId': self.sku_id,
+            'num': 1,
+            'addressId': default_address['id'],
+            'yuShou': 'true',
+            'isModifyAddress': 'false',
+            'name': default_address['name'],
+            'provinceId': default_address['provinceId'],
+            'cityId': default_address['cityId'],
+            'countyId': default_address['countyId'],
+            'townId': default_address['townId'],
+            'addressDetail': default_address['addressDetail'],
+            'mobile': default_address['mobile'],
+            'mobileKey': default_address['mobileKey'],
+            'email': default_address.get('email', ''),
+            'postCode': '',
+            'invoiceTitle': invoice_info.get('invoiceTitle', -1),
+            'invoiceCompanyName': '',
+            'invoiceContent': invoice_info.get('invoiceContentType', 1),
+            'invoiceTaxpayerNO': '',
+            'invoiceEmail': '',
+            'invoicePhone': invoice_info.get('invoicePhone', ''),
+            'invoicePhoneKey': invoice_info.get('invoicePhoneKey', ''),
+            'invoice': 'true' if invoice_info else 'false',
+            'password': '',
+            'codTimeType': 3,
+            'paymentType': 4,
+            'areaCode': '',
+            'overseas': 0,
+            'phone': '',
+            'eid': global_config.getRaw('config', 'eid'),
+            'fp': global_config.getRaw('config', 'fp'),
+            'token': token,
+            'pru': ''
+        }
+        return data
+
+    def submit_seckill_order(self):
+        """提交抢购（秒杀）订单
+        :param sku_id: 商品id
+        :param num: 购买数量，可选参数，默认1个
+        :return: 抢购结果 True/False
+        """
+        url = 'https://marathon.jd.com/seckillnew/orderService/pc/submitOrder.action'
+        payload = {
+            'skuId': self.sku_id,
+        }
+        self.seckill_order_data[self.sku_id] = self._get_seckill_order_data(
+            )
+        logger.info('提交抢购订单...')
+        headers = {
+            'User-Agent': DEFAULT_USER_AGENT,
+            'Host': 'marathon.jd.com',
+            'Referer': 'https://marathon.jd.com/seckill/seckill.action?skuId={0}&num={1}&rid={2}'.format(
+                self.sku_id, 1, int(time.time())),
+        }
+        resp = self.session.post(
+            url=url,
+            params=payload,
+            data=self.seckill_order_data.get(
+                self.sku_id),
+            headers=headers)
+        resp_json = parse_json(resp.text)
+        # 返回信息
+        # 抢购失败：
+        # {'errorMessage': '很遗憾没有抢到，再接再厉哦。', 'orderId': 0, 'resultCode': 60074, 'skuId': 0, 'success': False}
+        # {'errorMessage': '抱歉，您提交过快，请稍后再提交订单！', 'orderId': 0, 'resultCode': 60017, 'skuId': 0, 'success': False}
+        # {'errorMessage': '系统正在开小差，请重试~~', 'orderId': 0, 'resultCode': 90013, 'skuId': 0, 'success': False}
+        # 抢购成功：
+        # {"appUrl":"xxxxx","orderId":820227xxxxx,"pcUrl":"xxxxx","resultCode":0,"skuId":0,"success":true,"totalMoney":"xxxxx"}
+        if resp_json.get('success'):
+            order_id = resp_json.get('orderId')
+            total_money = resp_json.get('totalMoney')
+            pay_url = 'https:' + resp_json.get('pcUrl')
+            logger.info(
+                '抢购成功，订单号: %s, 总价: %s, 电脑端付款链接: %s',
+                order_id,
+                total_money,
+                pay_url)
+            return True
+        else:
+            logger.info('抢购失败，返回信息: %s', resp_json)
+            return False
---
+++ util.py
@@ -0,0 +1,231 @@
+import functools
+import json
+import random
+import re
+import sys
+import time
+import warnings
+from base64 import b64encode
+import requests
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+from jd_mask.jdlogger import logger
+from jd_mask.config import global_config
+from lxml import etree
+
+
+RSA_PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDC7kw8r6tq43pwApYvkJ5lalja
+N9BZb21TAIfT/vexbobzH7Q8SUdP5uDPXEBKzOjx2L28y7Xs1d9v3tdPfKI2LR7P
+AzWBmDMn8riHrDDNpUpJnlAGUqJG9ooPn8j7YNpcxCa1iybOlc2kEhmJn5uwoanQ
+q+CA6agNkqly2H4j6wIDAQAB
+-----END PUBLIC KEY-----"""
+
+DEFAULT_TIMEOUT = 10
+
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+    "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
+    "Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1467.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.90 Safari/537.36",
+    "Mozilla/5.0 (X11; NetBSD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+    "Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.14 (KHTML, like Gecko) Chrome/24.0.1292.0 Safari/537.14"
+    ]
+
+def encrypt_pwd(password, public_key=RSA_PUBLIC_KEY):
+    rsa_key = RSA.importKey(public_key)
+    encryptor = Cipher_pkcs1_v1_5.new(rsa_key)
+    cipher = b64encode(encryptor.encrypt(password.encode('utf-8')))
+    return cipher.decode('utf-8')
+
+def encrypt_payment_pwd(payment_pwd):
+    return ''.join(['u3' + x for x in payment_pwd])
+
+def response_status(resp):
+    if resp.status_code != requests.codes.OK:
+        print('Status: %u, Url: %s' % (resp.status_code, resp.url))
+        return False
+    return True
+
+def save_image(resp, image_file):
+    with open(image_file, 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=1024):
+            f.write(chunk)
+
+def parse_json(s):
+    begin = s.find('{')
+    end = s.rfind('}') + 1
+    return json.loads(s[begin:end])
+
+def get_tag_value(tag, key='', index=0):
+    if key:
+        value = tag[index].get(key)
+    else:
+        value = tag[index].text
+    return value.strip(' \t\r\n')
+
+def parse_items_dict(d):
+    result = ''
+    for index, key in enumerate(d):
+        if index < len(d) - 1:
+            result = result + '{0} x {1}, '.format(key, d[key])
+        else:
+            result = result + '{0} x {1}'.format(key, d[key])
+    return result
+
+def parse_area_id(area):
+    """解析地区id字符串：将分隔符替换为下划线 _
+    :param area: 地区id字符串（使用 _ 或 - 进行分割），如 12_904_3375 或 12-904-3375
+    :return: 解析后字符串
+    """
+    area_id_list = list(map(lambda x: x.strip(), re.split('_|-', area)))
+    area_id_list.extend((4 - len(area_id_list)) * ['0'])
+    return '_'.join(area_id_list)
+
+def split_area_id(area):
+    """将地区id字符串按照下划线进行切割，构成数组。数组长度不满4位则用'0'进行填充。
+    :param area: 地区id字符串（使用 _ 或 - 进行分割），如 12_904_3375 或 12-904-3375
+    :return: list
+    """
+    area_id_list = list(map(lambda x: x.strip(), re.split('_|-', area)))
+    area_id_list.extend((4 - len(area_id_list)) * ['0'])
+    return area_id_list
+
+def deprecated(func):
+    """This decorator is used to mark functions as deprecated.
+    It will result in a warning being emitted when the function is used.
+    作废方法
+    """
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn(
+            "Call to deprecated function {}.".format(func.__name__),
+            category=DeprecationWarning,
+            stacklevel=2
+        )
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+    return new_func
+
+# def check_login(func):
+#     """用户登陆态校验装饰器。若用户未登陆，则调用扫码登陆"""
+#     @functools.wraps(func)
+#     def new_func(self, *args, **kwargs):
+#         if not self.is_login:
+#             logger.info("{0} 需登陆后调用，开始扫码登陆".format(func.__name__))
+#             self.login_by_QRcode()
+#         return func(self, *args, **kwargs)
+#     return new_func
+
+def get_random_useragent():
+    """生成随机的UserAgent
+    :return: UserAgent字符串
+    """
+    return random.choice(USER_AGENTS)
+
+def get_cookies():
+    manual_cookies = {}
+    for item in global_config.getRaw('config','cookies_String').split(';'):
+        name, value = item.strip().split('=', 1)
+        # 用=号分割，分割1次
+        manual_cookies[name] = value
+        # 为字典cookies添加内容
+    cookiesJar = requests.utils.cookiejar_from_dict(manual_cookies, cookiejar=None, overwrite=True)
+    return cookiesJar
+
+def get_session():
+    # 初始化session
+    session = requests.session()
+    session.headers = {"User-Agent": DEFAULT_USER_AGENT,
+                       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                       "Connection": "keep-alive"}
+    checksession = requests.session()
+    checksession.headers = {"User-Agent": DEFAULT_USER_AGENT,
+                            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                            "Connection": "keep-alive"}
+    # 获取cookies保存到session
+    session.cookies = get_cookies()
+    return session
+
+def get_sku_title():
+    url = 'https://item.jd.com/{}.html'.format(global_config.getRaw('config','sku_id'))
+    session = get_session()
+    resp = session.get(url).content
+    x_data = etree.HTML(resp)
+    sku_title = x_data.xpath('/html/head/title/text()')
+    return sku_title[0]
+
+def check_login(func):
+    """用户登陆态校验装饰器。若用户未登陆，则调用扫码登陆"""
+    @functools.wraps(func)
+    def new_func(self,*args, **kwargs):
+        session = get_session()
+        for flag in range(1, 3):
+            try:
+                targetURL = 'https://order.jd.com/center/list.action'
+                payload = {
+                    'rid': str(int(time.time() * 1000)),
+                }
+                resp = session.get(
+                    url=targetURL, params=payload, allow_redirects=False)
+                if resp.status_code == requests.codes.OK:
+                    logger.info('校验是否登录[成功]')
+                    return True
+                else:
+                    logger.info('校验是否登录[失败]')
+                    logger.info('请从新输入cookie')
+                    time.sleep(1)
+                    continue
+            except Exception as e:
+                logger.info('cookie登录失败请重新获取cookie')
+                time.sleep(1)
+                sys.exit(1)
+        return func(self, *args, **kwargs)
+    return new_func
